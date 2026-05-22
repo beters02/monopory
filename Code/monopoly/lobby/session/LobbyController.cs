1b97bb88cb247ff2b066782a938789db6b374659
Bryce Peters <brycepeters04@gmail.com>
2026-05-21 18:25:07 -0600

    deserialize useless config

--- Code/monopoly/lobby/session/LobbyController.cs
+++ Code/monopoly/lobby/session/LobbyController.cs
@@ -3,7 +3,7 @@ using System;
 
 public sealed partial class LobbyController : Component
 {
-	[Property] public MatchConfig Config { get; set; } = new();
+	public MatchConfig Config;
 	[Property, Sync] public int HostedMinPlayers { get; set; } = 1;
 	[Property, Sync] public int HostedMaxPlayers { get; set; } = 6;
 	[Property, Sync] public bool HostedOnlyHostStartsGame { get; set; } = true;
