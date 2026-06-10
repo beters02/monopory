bef367956551f16b5249e03da99e58e5cbaa489f
Bryce Peters <brycepeters04@gmail.com>
2026-06-09 21:22:47 -0600

    update game versio

--- Code/monopoly/app/MonopolyApp.cs
+++ Code/monopoly/app/MonopolyApp.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 public class MonopolyApp : Component
 {
 
-    public static string GameVersion = "0.0.94";
+    public static string GameVersion = "0.0.941";
     public static bool IsStandalone = false;
     public static bool IsDebugEnabled = false;
     public static bool GameLaunchedWithDebugConvar = false;
