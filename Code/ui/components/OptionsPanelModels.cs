5645c6361f1879c3964d6d85e30174965ded325d
Bryce Peters <brycepeters04@gmail.com>
2026-06-11 17:21:41 -0600

    options menu subsections implementation

--- Code/ui/components/OptionsPanelModels.cs
+++ Code/ui/components/OptionsPanelModels.cs
@@ -9,5 +9,15 @@ public sealed class OptionsPanelSection
 	public string EmptyTitle { get; init; } = "";
 	public string EmptyMessage { get; init; } = "";
 	public bool HasItems { get; init; }
+	public IReadOnlyList<OptionsPanelSubsection> Subsections { get; init; } = new List<OptionsPanelSubsection>();
 	public RenderFragment HeaderContent { get; init; }
+}
+
+public sealed class OptionsPanelSubsection
+{
+	public string Key { get; init; } = "";
+	public string Title { get; init; } = "";
+	public string EmptyTitle { get; init; } = "";
+	public string EmptyMessage { get; init; } = "";
+	public bool HasItems { get; init; }
 }
