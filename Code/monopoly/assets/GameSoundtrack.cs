6d4efba4a01643b66c051fea60b1364dd8930efe
Bryce Peters <brycepeters04@gmail.com>
2026-06-14 15:30:04 -0600

    fix json shit

--- Code/monopoly/assets/GameSoundtrack.cs
+++ Code/monopoly/assets/GameSoundtrack.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 public sealed class GameSoundtrack : GameSoundBase<GameSoundtrack>
 {
 
 	public float FadeInTime { get; set; } = 0;
 	public float Delay { get; set; } = 0;
 
+	[JsonConstructor]
 	public GameSoundtrack( string path ) : base( path ) { }
 	public GameSoundtrack ( string path, float delay = 0, float fadeInTime = 0 ) : base ( path )
 	{
