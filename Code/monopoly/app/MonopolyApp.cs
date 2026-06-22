e4c24e9095612a12276746161dee8a5fc90c45d2
Bryce Peters <brycepeters04@gmail.com>
2026-06-21 18:16:01 -0600

    update game version

--- Code/monopoly/app/MonopolyApp.cs
+++ Code/monopoly/app/MonopolyApp.cs
@@ -10,7 +10,7 @@ public enum InputActionIconType
 public partial class MonopolyApp : Component
 {
 
-    public static readonly string GameVersion = "0.0.95";
+    public static readonly string GameVersion = "0.0.952";
     public static readonly ulong AppId = 4745160;
     
     private static bool Standalone = false;
