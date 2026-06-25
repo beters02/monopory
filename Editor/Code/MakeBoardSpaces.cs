989813f20d2bc1485c82ac8dc2661490293ad4f6
Bryce Peters <brycepeters04@gmail.com>
2026-06-25 17:31:29 -0600

    init makeboardspaces editor tool

---
+++ Editor/Code/MakeBoardSpaces.cs
@@ -0,0 +1,21 @@
+public class MakeBoardSpaces
+{
+    
+    [ConCmd("temp_make_board_spaces")]
+    public static void TempBoardSpaces()
+    {
+        
+        Dictionary<string, List<string>> Sections = [
+            
+        ];
+
+        Dictionary<string, List<string>> Connectors =
+        [
+            
+        ];
+
+    }
+
+
+
+}
